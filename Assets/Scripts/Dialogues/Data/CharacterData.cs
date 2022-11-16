07dc6bc97f3f98f2169af0719aebabd111f518ec
mnicolas94 <miguelcok27@gmail.com>
2022-11-16 08:36:21 -0500

    FEAT: setup dialogues and dialogues tests scene

---
+++ Assets/Scripts/Dialogues/Data/CharacterData.cs
@@ -0,0 +1,18 @@
+﻿﻿using UnityEngine;
+
+ namespace Dialogues.Data
+{
+    [CreateAssetMenu(fileName = "CharacterData", menuName = "Characters/CharacterData", order = 0)]
+    public class CharacterData : ScriptableObject
+    {
+        [SerializeField] private string _characterName;
+        [SerializeField] private Color _textColor;
+        [SerializeField] private Sprite _characterSprite;
+
+        public string CharacterName => _characterName;
+
+        public Color TextColor => _textColor;
+
+        public Sprite CharacterSprite => _characterSprite;
+    }
+}
---
+++ Assets/Scripts/Dialogues/Data/CustomDialogueLineData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Localization;
+
+namespace Dialogues.Data
+{
+    [Serializable]
+    public class CustomDialogueLineData : IDialogueLineData
+    {
+        [SerializeField] private CharacterData _character;
+
+        [Space(12)]
+        [SerializeField] private LocalizedString _dialogueText;
+
+        public CharacterData Character => _character;
+
+        public LocalizedString DialogueText => _dialogueText;
+
+        public override string ToString()
+        {
+            return $"{_character.CharacterName}: {_dialogueText}";
+        }
+    }
+}
