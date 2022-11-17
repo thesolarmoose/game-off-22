c0e98ebb0b8ce17fa96a8e1e74c62e3107a2eba2
mnicolas94 <miguelcok27@gmail.com>
2022-11-17 11:55:59 -0500

    FEAT: add outline shader when hover interactable

--- Assets/Scripts/Interactions/OutlineShaderSetter.cs
+++ Assets/Scripts/Interactions/OutlineShaderSetter.cs
@@ -5,15 +5,17 @@ namespace Interactions
     public class OutlineShaderSetter : MonoBehaviour
     {
         [SerializeField] private SpriteRenderer _renderer;
+        [SerializeField] private Material _default;
+        [SerializeField] private Material _outline;
 
         public void SetShader()
         {
-            _renderer.color = Color.black;
+            _renderer.material = _outline;
         }
 
         public void RemoveShader()
         {
-            _renderer.color = Color.white;
+            _renderer.material = _default;
         }
     }
 }
