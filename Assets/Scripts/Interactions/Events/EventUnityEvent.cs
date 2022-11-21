a79f7752abf61cfa5429bc34d98d3ea9645e04eb
mnicolas94 <miguelcok27@gmail.com>
2022-11-21 10:50:08 -0500

    FEAT: add irene's lineart background

---
+++ Assets/Scripts/Interactions/Events/EventUnityEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Interactions.Events
+{
+    [Serializable]
+    public class EventUnityEvent : IInteractionEvent
+    {
+        [SerializeField] private UnityEvent _event;
+        
+        public async Task<bool> ExecuteEvent(CancellationToken ct)
+        {
+            _event.Invoke();
+
+            return true;
+        }
+    }
+}
