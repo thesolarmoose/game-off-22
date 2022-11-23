68b5c201130f612230f084f423f571435a1e9304
mnicolas94 <miguelcok27@gmail.com>
2022-11-23 08:56:12 -0500

    FEAT: add open inventory icon animation when item added to inventory

---
+++ Assets/Scripts/Items/ItemAddedAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BrunoMikoski.AnimationSequencer;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace Items
+{
+    public class ItemAddedAnimation : MonoBehaviour
+    {
+        [SerializeField] private ItemEvent _itemAddedEvent;
+        [SerializeField] private AnimationSequencerController _animation;
+
+        private void OnEnable()
+        {
+            _itemAddedEvent.Register(PlayAnimation);
+        }
+        
+        private void OnDisable()
+        {
+            _itemAddedEvent.Unregister(PlayAnimation);
+        }
+
+        private void PlayAnimation()
+        {
+            _animation.Play();
+        }
+    }
+}
