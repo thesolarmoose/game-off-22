a8f9402d5b631a063646cd0565d762b2b1304485
mnicolas94 <miguelcok27@gmail.com>
2022-11-25 09:12:20 -0500

    FEAT: parallax FIX: fix dialogue text not displaying in webgl builds

---
+++ Assets/Scripts/Parallax/ParallaxPosition.cs
@@ -0,0 +1,31 @@
+﻿﻿using UnityEngine;
+
+ namespace Parallax
+{
+    public class ParallaxPosition : MonoBehaviour
+    {
+        [SerializeField] private Transform _camera;
+    
+        [SerializeField] private float _horizontalSpeed;
+        [SerializeField] private float _verticalSpeed;
+
+        private Vector3 _cameraIntersectionPosition;
+    
+        void Awake () {
+	        _cameraIntersectionPosition = transform.position;
+        }
+        
+        void Update ()
+        {
+	        var center = _cameraIntersectionPosition;
+	        var cameraPosition = _camera.position;
+	        
+	        var offset = center - cameraPosition;
+	        var newPosX = center.x + offset.x * _horizontalSpeed;
+	        var newPosY = center.y + offset.y * _verticalSpeed;
+	        var newPos = new Vector3(newPosX, newPosY, center.z);
+	        
+	        transform.position = newPos;
+        }
+    }
+}
