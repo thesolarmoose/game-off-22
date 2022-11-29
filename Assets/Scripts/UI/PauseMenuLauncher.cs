3659a10cc6ebef0c104dacbc4d60f98c797c0f4f
mnicolas94 <miguelcok27@gmail.com>
2022-11-29 15:39:13 -0500

    FEAT: add pause menu

---
+++ Assets/Scripts/UI/PauseMenuLauncher.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AsyncUtils;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace UI
+{
+    public class PauseMenuLauncher : MonoBehaviour
+    {
+        [SerializeField] private AsyncPopup _pauseMenuPopup;
+        [SerializeField] private InputAction _pauseAction;
+
+        [SerializeField] private UnityEvent _onBeforePause;
+        [SerializeField] private UnityEvent _onAfterPause;
+
+        private CancellationTokenSource _cts;
+        private bool _paused;
+
+        private async void ShowPauseMenu(InputAction.CallbackContext ctx)
+        {
+            if (!_paused)
+            {
+                _paused = true;
+                _onBeforePause.Invoke();
+                _cts = new CancellationTokenSource();
+                var ct = _cts.Token;
+                await Popups.ShowPopup(_pauseMenuPopup, ct);    
+                _paused = false;
+                _onAfterPause.Invoke();
+            }
+            else
+            {
+                await Task.Yield();
+                Cancel();
+            }
+        }
+
+        private void Start()
+        {
+            _pauseAction.Enable();
+            _pauseAction.performed += ShowPauseMenu;
+        }
+
+        private void OnEnable()
+        {
+            _pauseAction?.Enable();
+        }
+
+        private void OnDisable()
+        {
+            Cancel();
+            _pauseAction?.Disable();
+        }
+
+        private void Cancel()
+        {
+            if (!_cts.IsCancellationRequested)
+            {
+                _cts.Cancel();
+            }
+
+            _cts.Dispose();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PauseMenuPopup.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AsyncUtils;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class PauseMenuPopup : AsyncPopup
+    {
+        [SerializeField] private Button _resumeButton;
+        
+        public override async Task Show(CancellationToken ct)
+        {
+            await AsyncUtils.Utils.WaitPressButtonAsync(_resumeButton, ct);
+        }
+
+        public override void Initialize()
+        {
+        }
+    }
+}
