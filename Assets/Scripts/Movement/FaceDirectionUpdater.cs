7914f84cdef58375b7a5319f0c52ad093c214303
mnicolas94 <miguelcok27@gmail.com>
2022-11-30 09:09:56 -0500

    FEAT: change scale of character based on movement direction

---
+++ Assets/Scripts/Movement/FaceDirectionUpdater.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Pathfinding;
+using UnityEngine;
+
+namespace Movement
+{
+    public class FaceDirectionUpdater : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private AIPath _mover;
+        [SerializeField] private bool _invert;
+
+        private void Update()
+        {
+            var horizontalVelocity = _mover.desiredVelocity.x;
+            var sign = Mathf.Sign(horizontalVelocity);
+            var invertFactor = _invert ? -1 : 1;
+            sign *= invertFactor;
+
+            _target.localScale = new Vector3(sign, 1, 1);
+        }
+    }
+}
