dd038231b17e5df1e6cf231279d4b7b839ea88be
mnicolas94 <miguelcok27@gmail.com>
2022-11-30 18:52:57 -0500

    FEAT: add steps sounds

---
+++ Assets/Scripts/Movement/SoundSteps.cs
@@ -0,0 +1,15 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+namespace Movement
+{
+    public class SoundSteps : MonoBehaviour
+    {
+        [SerializeField] private StudioEventEmitter _emitter;
+
+        public void PlayStepSound()
+        {
+            _emitter.Play();
+        }
+    }
+}
